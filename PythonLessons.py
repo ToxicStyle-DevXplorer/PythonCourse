09ebb198b88a22009139853637acd735cb6887a3
ToxicStyle-DevXplorer <156326226+ToxicStyle-DevXplorer@users.noreply.github.com>
2024-08-25 23:55:29 +0500

    Add files via upload

---
+++ PythonLessons.py
@@ -0,0 +1,11 @@
+integer = 2 + 3 * 5
+print(integer)
+
+my_integer = 4
+my_float = 2.5
+summary = my_float + my_integer
+print(summary)
+
+my_float = float(input('input the number: '))
+my_int = int(my_float)
+print(my_int)
